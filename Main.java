219a7eb6795703e34f6e64795b4777dab3bf87be
younj1601 <younj1601@wrdsb.ca>
2020-10-14 16:36:56 +0000

    Finished the fourth task for 2.05

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
 /**
- *
- * @author 
+ *Returns the last digit of a number
+ * @author Jackson 
  */
 public class Main {
+
+  public static int firstDigit(int num){
+    //calculating the last digit
+    int lastNum = num % 10;
+    //returning the answer
+    return lastNum;
+
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //
+    int lastNum = firstDigit(3572);
+    //print answer
+    System.out.println(lastNum);
   }
 }
